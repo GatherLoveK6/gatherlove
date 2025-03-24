f11a8ccfa338ccffa8fd5028324acf59e124fa84
muzkii <andriyo.averill@ui.ac.id>
2025-03-24 15:47:38 +0700

    [FEAT] Initial Commit

---
+++ src/main/java/k6/gatherlove/GatherloveApplication.java
@@ -0,0 +1,13 @@
+package k6.gatherlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatherloveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatherloveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/k6/gatherlove/GatherloveApplicationTests.java
@@ -0,0 +1,13 @@
+package k6.gatherlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatherloveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
