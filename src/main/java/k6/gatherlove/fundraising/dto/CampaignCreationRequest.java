f6f4305e1e5bb19e1a547e20a245c54a194e5358
muzkii <andriyo.averill@ui.ac.id>
2025-04-11 17:17:46 +0700

    [GREEN] Implemented and changed some of the private data types on CampaignCreationRequest

--- src/main/java/k6/gatherlove/fundraising/dto/CampaignCreationRequest.java
+++ src/main/java/k6/gatherlove/fundraising/dto/CampaignCreationRequest.java
@@ -2,12 +2,17 @@ package k6.gatherlove.fundraising.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class CampaignCreationRequest {
     private String title;
     private String description;
-    private double goalAmount;
-    private String deadline;
+    private BigDecimal goalAmount;
+    private LocalDate deadline;
 }
