97b5dfa4858102192227a73697aa5931f1fff7d0
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2025-04-11 17:11:04 +0700

    added model for Auth.java

---
+++ src/main/java/k6/gatherlove/auth/model/User.java
@@ -0,0 +1,22 @@
+package k6.gatherlove.auth.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
