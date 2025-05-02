9cb8ce15fc0046d7539461d5a82c97a89e915c2f
Hafizh-Zen <hafizh.suryamustafa@gmail.com>
2025-05-02 20:03:03 +0700

    [GREEN] Implement User entity with Lombok and JPA annotations

--- src/main/java/k6/gatherlove/auth/model/User.java
+++ src/main/java/k6/gatherlove/auth/model/User.java
@@ -1,10 +1,16 @@
 package k6.gatherlove.auth.model;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Entity
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
