3a900d378ad04cfa5552046ccdf837f1e9b6c3a5
Geordie Vannese <geordievannese01@gmail.com>
2025-05-07 13:53:17 +0700

    [RED] High Level Networking Add TopUpRequest DTO to represent top-up amount and payment method ID

---
+++ src/test/java/k6/gatherlove/dto/TopUpRequest.java
@@ -0,0 +1,14 @@
+package k6.gatherlove.dto;
+
+import java.math.BigDecimal;
+
+public class TopUpRequest {
+    private BigDecimal amount;
+    private String paymentMethodId;
+
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+
+    public String getPaymentMethodId() { return paymentMethodId; }
+    public void setPaymentMethodId(String paymentMethodId) { this.paymentMethodId = paymentMethodId; }
+}
