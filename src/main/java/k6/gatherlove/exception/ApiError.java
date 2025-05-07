aa39463ff9916f15e7603fca95e2d35605ee096f
Geordie Vannese <geordievannese01@gmail.com>
2025-05-07 14:01:11 +0700

    [GREEN] Add ApiError class to encapsulate HTTP status codes and error messages for API responses, High Level Networking

---
+++ src/main/java/k6/gatherlove/exception/ApiError.java
@@ -0,0 +1,16 @@
+package k6.gatherlove.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiError {
+    private HttpStatus status;
+    private String message;
+
+    public ApiError(HttpStatus status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public HttpStatus getStatus() { return status; }
+    public String getMessage() { return message; }
+}
