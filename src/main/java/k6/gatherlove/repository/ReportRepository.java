6bfb8f97d376e967a382dc4d46a24b3f851c12db
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2025-05-08 17:16:40 +0700

    [GREEN] implement report repository

--- src/main/java/k6/gatherlove/repository/ReportRepository.java
+++ src/main/java/k6/gatherlove/repository/ReportRepository.java
@@ -2,7 +2,8 @@ package k6.gatherlove.repository;
 
 import k6.gatherlove.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
-    // Add custom query methods
+    List<Report> findByReportedBy(String reportedBy);
 }
