f5e77c25644dc21a02a33274649690cc6fd0968d
Kevin Adriano <kevin.adriano@cs.ui.ac.id>
2025-05-18 10:25:13 +0700

    [GREEN] implement changes in report repository

--- src/main/java/k6/gatherlove/repository/ReportRepository.java
+++ src/main/java/k6/gatherlove/repository/ReportRepository.java
@@ -3,7 +3,8 @@ package k6.gatherlove.repository;
 import k6.gatherlove.model.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.UUID;
 
-public interface ReportRepository extends JpaRepository<Report, Long> {
+public interface ReportRepository extends JpaRepository<Report, UUID> {
     List<Report> findByReportedBy(String reportedBy);
 }
