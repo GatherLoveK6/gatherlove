31571e8f86782becbe2b4b488d79f3bad3a4a6a3
muzkii <andriyo.averill@ui.ac.id>
2025-05-25 13:09:12 +0700

    [REFACTOR] Making the AdminCampaignStatusControllerTest.java to succeed

--- src/test/java/k6/gatherlove/AdminDashboard/controller/AdminCampaignStatusControllerTest.java
+++ src/test/java/k6/gatherlove/AdminDashboard/controller/AdminCampaignStatusControllerTest.java
@@ -4,30 +4,34 @@ import k6.gatherlove.AdminDashboard.dto.Campaign;
 import k6.gatherlove.AdminDashboard.service.AdminCampaignService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(AdminCampaignController.class) // you're likely using the same controller
+@ExtendWith(MockitoExtension.class)
 public class AdminCampaignStatusControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
+    @Mock
     private AdminCampaignService adminCampaignService;
 
+    @InjectMocks
+    private AdminCampaignController adminCampaignController;
+
+    private MockMvc mockMvc;
     private Campaign updatedCampaign;
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(adminCampaignController).build();
         updatedCampaign = Campaign.builder()
                 .id(1L)
                 .title("Bantu Sekolah")
