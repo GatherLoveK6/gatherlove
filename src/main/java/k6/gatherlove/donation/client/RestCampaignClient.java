3ff7c2d5004e001b6885a04ef64db65ef6ab14ca
adienanimeesha <adiena.nimeesha@gmail.com>
2025-05-25 15:44:05 +0700

    [REFACTOR] updated RestWalletClient & RestCampaignClient to use new config

--- src/main/java/k6/gatherlove/donation/client/RestCampaignClient.java
+++ src/main/java/k6/gatherlove/donation/client/RestCampaignClient.java
@@ -1,8 +1,6 @@
 package k6.gatherlove.donation.client;
 
-import k6.gatherlove.donation.client.CampaignClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,30 +8,51 @@ import java.util.Map;
 
 @Service
 public class RestCampaignClient implements CampaignClient {
-    private final RestTemplate rest;
 
-    @Value("${fundraising.service.url:http://localhost:8082}")
-    private String fundraisingBaseUrl;
+    private final RestTemplate restTemplate;
 
-    public RestCampaignClient(RestTemplateBuilder builder) {
-        this.rest = builder.build();
+    /**
+     * Base URL for the fundraising service. If the property
+     * "fundraising.service.url" is not set, this will default
+     * to "http://localhost:8080".
+     */
+    @Value("${fundraising.service.url:http://localhost:8080}")
+    private String baseUrl;
+
+    public RestCampaignClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
+    /**
+     * Notify the fundraising service of a successful donation.
+     *
+     * @param campaignId ID of the campaign
+     * @param donationId ID of the donation record
+     * @param amount     donation amount
+     */
     @Override
     public void recordDonation(String campaignId, String donationId, double amount) {
-        rest.postForEntity(
-                fundraisingBaseUrl + "/campaigns/{campaignId}/donations",
-                Map.of("donationId", donationId, "amount", amount),
-                Void.class,
-                campaignId
+        String url = String.format("%s/api/fundraising/campaigns/%s/donations", baseUrl, campaignId);
+
+        Map<String, Object> payload = Map.of(
+                "donationId", donationId,
+                "amount",     amount
         );
+
+        restTemplate.postForEntity(url, payload, Void.class);
     }
 
+    /**
+     * Notify the fundraising service to remove (cancel) a donation.
+     *
+     * @param campaignId ID of the campaign
+     * @param donationId ID of the donation to cancel
+     */
     @Override
     public void removeDonation(String campaignId, String donationId) {
-        rest.delete(
-                fundraisingBaseUrl + "/campaigns/{campaignId}/donations/{donationId}",
-                campaignId, donationId
-        );
+        String url = String.format("%s/api/fundraising/campaigns/%s/donations/%s",
+                baseUrl, campaignId, donationId);
+
+        restTemplate.delete(url);
     }
 }
