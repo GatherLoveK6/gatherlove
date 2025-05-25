124cc0cabdd6cf0e06b79080b49cb70cdc3daf1d
muzkii <andriyo.averill@ui.ac.id>
2025-05-25 13:11:31 +0700

    [REFACTOR] Making the HelloControllerTest.java to succeed

--- src/test/java/k6/gatherlove/controller/HelloControllerTest.java
+++ src/test/java/k6/gatherlove/controller/HelloControllerTest.java
@@ -1,18 +1,30 @@
 package k6.gatherlove.controller;
 
+import k6.gatherlove.auth.filter.JwtAuthenticationFilter;
+import k6.gatherlove.auth.util.JwtUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(HelloController.class)
+@AutoConfigureMockMvc(addFilters = false)  // Disable security filters to avoid dependency issues
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    // Mock the JWT dependencies that would otherwise cause issues
+    @MockBean
+    private JwtUtil jwtUtil;
+    
+    @MockBean 
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Test
     public void testHelloEndpoint() throws Exception {
