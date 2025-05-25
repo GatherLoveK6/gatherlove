5eb8406d2a5afcbbbd4c890c0bea8ae85ab986af
muzkii <andriyo.averill@ui.ac.id>
2025-05-25 13:08:52 +0700

    [REFACTOR] Making the AdminCampaignControllerTest.java to succeed

--- src/test/java/k6/gatherlove/AdminDashboard/controller/AdminCampaignControllerTest.java
+++ src/test/java/k6/gatherlove/AdminDashboard/controller/AdminCampaignControllerTest.java
@@ -4,12 +4,13 @@ import k6.gatherlove.AdminDashboard.service.AdminCampaignService;
 import k6.gatherlove.AdminDashboard.dto.Campaign;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
@@ -17,19 +18,21 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(AdminCampaignController.class)
+@ExtendWith(MockitoExtension.class)
 public class AdminCampaignControllerTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    @MockBean
+    @Mock
     private AdminCampaignService adminCampaignService;
 
+    @InjectMocks
+    private AdminCampaignController adminCampaignController;
+
+    private MockMvc mockMvc;
     private Campaign sampleCampaign;
 
     @BeforeEach
     void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(adminCampaignController).build();
         sampleCampaign = Campaign.builder()
                 .id(1L)
                 .title("Bantu Sekolah")
